2b8596b9735490186f8af57ded46425463c71097
haruto0529 <harutonan@yahoo.co.jp>
2026-05-10 15:14:20 +0900

    spring boot environment construction

---
+++ you-say-app/src/main/java/com/example/you_say_app/YouSayAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.you_say_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouSayAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YouSayAppApplication.class, args);
+	}
+
+}
---
+++ you-say-app/src/test/java/com/example/you_say_app/YouSayAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.you_say_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouSayAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
