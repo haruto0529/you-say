e11727cdd20cda6f7c6010763761225a1e304b20
haruto0529 <harutonan@yahoo.co.jp>
2026-06-14 11:08:25 +0900

    quotesDTOとDAOを作成

---
+++ you-say-app/src/main/java/com/example/you_say_app/model/dao/QuotesDao.java
@@ -0,0 +1,13 @@
+package com.example.you_say_app.model.dao;
+
+import com.example.you_say_app.model.dto.QuoteDto;
+
+public class QuotesDao {
+	public QuoteDto getQuotes(int userId) {
+		QuoteDto items = new QuoteDto();
+		String sql = "";
+		return items;
+		
+	}
+
+}
