961e4fec5bb8240adf5549d69cfd433389dfe2b9
haruto0529 <harutonan@yahoo.co.jp>
2026-06-14 11:50:04 +0900

    quotesDaoからCollectionDaoに変更、コレクション画面をmainのものに変更

--- you-say-app/src/main/java/com/example/you_say_app/model/dao/CollectionDao.java
+++ you-say-app/src/main/java/com/example/you_say_app/model/dao/CollectionDao.java
@@ -2,7 +2,7 @@ package com.example.you_say_app.model.dao;
 
 import com.example.you_say_app.model.dto.QuoteDto;
 
-public class QuotesDao {
+public class CollectionDao {
 	public QuoteDto getQuotes(int userId) {
 		QuoteDto items = new QuoteDto();
 		String sql = "";
