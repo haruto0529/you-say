29556b472aff4e503e1ed9d4249a8b18c1645728
haruto0529 <harutonan@yahoo.co.jp>
2026-06-19 18:43:30 +0900

    コレクションのDBに保存するためのDAOを作成

--- you-say-app/src/main/java/com/example/you_say_app/controller/CollectionController.java
+++ you-say-app/src/main/java/com/example/you_say_app/controller/CollectionController.java
@@ -1,14 +1,21 @@
 package com.example.you_say_app.controller;
 
+import jakarta.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.you_say_app.model.dto.UserDto;
 
 @Controller
 public class CollectionController {
 	@GetMapping("/collection")
-	public String collection() {
-		
-		return"collection";
+	public String showCollection(HttpSession session) {
+		UserDto loginUser = (UserDto) session.getAttribute("loginUser");
+		if (loginUser == null) {
+			return "redirect:/top";
+		}
+		return "collection";
 	}
 
 }
--- you-say-app/src/main/java/com/example/you_say_app/model/dao/CollectionDao.java
+++ you-say-app/src/main/java/com/example/you_say_app/model/dao/CollectionDao.java
@@ -1,13 +1,24 @@
 package com.example.you_say_app.model.dao;
 
-import com.example.you_say_app.model.dto.QuoteDto;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
-public class CollectionDao {
-	public QuoteDto getQuotes(int userId) {
-		QuoteDto items = new QuoteDto();
-		String sql = "";
-		return items;
-		
+public class CollectionDao extends SuperDao {
+	public int insartCollection(int user_id, int quote_id) {
+		int ret = 0;
+		String sql = "INSERT INTO `you_say`.`collecton_quotes` (user_id, `quote_id, has_gold_medal) VALUES (?, ?, 0)";
+
+		try (Connection con = getConnection();
+				PreparedStatement ps = con.prepareStatement(sql)) {
+			ps.setInt(1, user_id);
+			ps.setInt(2, quote_id);
+
+			ret = ps.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return ret;
 	}
 
 }
