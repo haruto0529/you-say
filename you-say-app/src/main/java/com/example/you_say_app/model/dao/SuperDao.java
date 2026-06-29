c2eef492da82664ac95d1be13b133588b9183cf9
haruto0529 <harutonan@yahoo.co.jp>
2026-06-30 08:43:41 +0900

    DBをローカルに変更

--- you-say-app/src/main/java/com/example/you_say_app/model/dao/SuperDao.java
+++ you-say-app/src/main/java/com/example/you_say_app/model/dao/SuperDao.java
@@ -4,23 +4,49 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
-
-@Component
+/**
+ * SuperDao - データベース接続の取得を担当する基底クラス
+ */
 public class SuperDao {
 
-	@Value("${my.db.url}")
-	private String dbUrl;
+    // 接続情報は本来外部ファイル化すべきですが、教育上の簡略化のため直接記述します
+    private static final String DB_URI =
+    		"jdbc:mysql://localhost:3306/you_say?characterEncoding=utf8&"
+            + "useSSL=false&serverTimezone=GMT%2B9&"
+            + "rewriteBatchedStatements=true";
+    private static final String DB_USER = "root";
+    private static final String DB_PASS = "";
 
-	@Value("${my.db.user}")
-	private String dbUser;
+    /**
+     * データベース接続を取得する
+     * 呼び出し元で try-with-resources を使用して確実に close することを想定
+     * @return データベース接続オブジェクト
+     * @throws SQLException 接続失敗時にスロー
+     */
+    protected Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(DB_URI, DB_USER, DB_PASS);
+    }
+    
+    public static void main(String[] args) {
+SuperDao dao = new SuperDao();
+        
+        System.out.println("テストを開始します...");
 
-	@Value("${my.db.pass}")
-	private String dbPass;
+        // try-with-resources 文を使用して接続テストを実施
+        try (Connection con = dao.getConnection()) {
+            
+            if (con != null && !con.isClosed()) {
+                System.out.println("成功：正常に接続されました！");
+                System.out.println("接続先: " + DB_URI);
+            }
 
-	protected Connection getConnection() throws SQLException {
-		return DriverManager.getConnection(dbUrl, dbUser, dbPass);
-	}
-
+        } catch (SQLException e) {
+            System.err.println("失敗：エラーが発生しました。");
+            System.err.println("原因: " + e.getMessage());
+            e.printStackTrace();
+        }
+        
+        // tryブロックを抜けると自動的に con.close() が呼ばれる
+        System.out.println("接続は自動的に切断されました");
+    }
 }
